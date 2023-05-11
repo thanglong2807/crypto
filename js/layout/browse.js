b08757d3d95090a2fcd5609cc658742a9b68d53e
Lê Thăng Long <long2323123@gmail.com>
2023-05-11 12:02:29 +0700

    push

--- js/layout/browse.js
+++ js/layout/browse.js
@@ -2,43 +2,43 @@ const browse =[
     {
         icon: './images/icon/Photo & Icon1.svg',
         name: 'Art',
-        img: '../images/img/Photo\ &\ Icon.png'
+        img: '../images/img/PhotoIcon.png'
     },
     {
         icon: './images/icon/Photo & Icon2.svg',
         name: 'Collectibles',
-        img: '../images/img/Photo\ &\ Icon2.png'
+        img: '../images/img/PhotoIcon2.png'
     },
   
     {
         icon: './images/icon/Photo & Icon3.svg',
         name: 'Music',
-        img: '../images/img/Photo\ &\ Icon3.png'
+        img: '../images/img/PhotoIcon3.png'
     },
     {
         icon: './images/icon/Photo & Icon4.svg',
         name: 'Photography',
-        img: '../images/img/Photo\ &\ Icon4.png'
+        img: '../images/img/PhotoIcon4.png'
     },
     {
         icon: './images/icon/Photo & Icon5.svg',
         name: 'Video',
-        img: '../images/img/Photo\ &\ Icon5.png'
+        img: '../images/img/PhotoIcon5.png'
     },
     {
         icon: './images/icon/Photo & Icon6.svg',
         name: 'Utility',
-        img: '../images/img/Photo\ &\ Icon6.png'
+        img: '../images/img/PhotoIcon6.png'
     },
     {
         icon: './images/icon/Photo & Icon7.svg',
         name: 'Sport',
-        img: '../images/img/Photo\ &\ Icon7.png'
+        img: '../images/img/PhotoIcon7.png'
     },
     {
         icon: './images/icon/Photo & Icon8.svg',
         name: 'Virtual Worlds',
-        img: '../images/img/Photo\ &\ Icon8.png'
+        img: '../images/img/PhotoIcon8.png'
     },
 ]
 const newBrwse = browse.map((item)=>{
