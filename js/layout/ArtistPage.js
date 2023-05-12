9e0c7e413f1f9ffdf82c3a7b843d2a9569ce6aae
Lê Thăng Long <long2323123@gmail.com>
2023-05-12 16:10:47 +0700

    update

--- js/layout/ArtistPage.js
+++ js/layout/ArtistPage.js
@@ -55,34 +55,40 @@ const artistPage = [
     },
 
 ]
+const myMap = (arr)=>{
+    const newArtistPage = arr.map(item => {
+        return `
+        <div class="discovermore__item">
+        <a href='' style='display: flex;
+        flex-direction: column;'>
+            <img   class="discovermore__item-img" src="images/img/${item.image}" alt="">
+            <div class="discovermore__item-botton">
+                <h3 class="discovermore__item-name--img">${item.name}</h3>
+                <div class="discovermore__item-info">
+                    <img   src="images/img/Avatar_discover1.png" alt=""
+                        class="discovermore__item-avatar">
+                    <h5 class="discovermore__item-name">Animakid</h5>
+                </div>
+                <div class="discovermore__item-price">
+                    <div class="discovermore--price-item">
+                        <span class="discovermore--price-name">Price</span>
+                        <h6 class="discovermore--price">${item.price}</h6>
+                    </div>
+                    <div class="discovermore--price-item">
+                        <span class="discovermore--price-name">Highest Bid</span>
+                        <h6 class="discovermore--price">${item.HighestBid}</h6>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+        `
+    })
+  return  $('.discovermore__list').html(newArtistPage)
 
-const newArtistPage = artistPage.map(item => {
-    return `
-    <div class="discovermore__item">
-    <a href='' style='display: flex;
-    flex-direction: column;'>
-        <img   class="discovermore__item-img" src="images/img/${item.image}" alt="">
-        <div class="discovermore__item-botton">
-            <h3 class="discovermore__item-name--img">${item.name}</h3>
-            <div class="discovermore__item-info">
-                <img   src="images/img/Avatar_discover1.png" alt=""
-                    class="discovermore__item-avatar">
-                <h5 class="discovermore__item-name">Animakid</h5>
-            </div>
-            <div class="discovermore__item-price">
-                <div class="discovermore--price-item">
-                    <span class="discovermore--price-name">Price</span>
-                    <h6 class="discovermore--price">${item.price}</h6>
-                </div>
-                <div class="discovermore--price-item">
-                    <span class="discovermore--price-name">Highest Bid</span>
-                    <h6 class="discovermore--price">${item.HighestBid}</h6>
-                </div>
-            </div>
-        </div>
-    </a>
-</div>
-    `
+}
+
+myMap(artistPage)
+$('.creacted-handle-btn').on('click', function(e) {
+    myMap(artistPage.reverse())
 })
-$('.discovermore__list').html(newArtistPage)
-
